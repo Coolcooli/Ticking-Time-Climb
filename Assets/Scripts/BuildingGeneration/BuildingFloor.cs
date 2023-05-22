491c764bb1a5ca146703c98fe0669bbdb70362e0
Lars Nienhuis <lars.nienhuis@hva.nl>
2023-05-22 22:54:51 +0200

    fix floor pos

--- Assets/Scripts/BuildingGeneration/BuildingFloor.cs
+++ Assets/Scripts/BuildingGeneration/BuildingFloor.cs
@@ -7,16 +7,17 @@ public class BuildingFloor : MonoBehaviour
     private Tile[] _tiles;
 
     private const float TILE_WIDTH = .48f;
-    private const float TILE_OFFSET = -.5f;
 
     public void InitializeFloor(Tile tile, int floorWidth)
     {
         _tiles = new Tile[floorWidth];
 
+        float tileOffset = (_tiles.Length - 1) * TILE_WIDTH / 2;
+
         for (int i = 0; i < _tiles.Length; i++)
         {
             Tile newTile = Instantiate(tile, transform);
-            newTile.transform.position = new Vector2(i * TILE_WIDTH + TILE_OFFSET, transform.position.y);
+            newTile.transform.position = new Vector2(i * TILE_WIDTH - tileOffset, transform.position.y);
             _tiles[i] = newTile;
         }
     }
